5fbdd126b4865a214e0d21f234e08cef0a46bfe1
daisuke-lunas <tamura.lunas@gmail.com>
2025-07-12 17:21:42 +0900

    スタッフの勤務記録を取るSQL

---
+++ app/sql/staff-checkin.sql
@@ -0,0 +1,14 @@
+SELECT 
+    users.id, 
+    users.username, 
+    checkins.type, 
+    DATE_ADD(checkins.check_in_at, INTERVAL 9 HOUR) AS check_in_at_jst
+FROM 
+    checkins
+JOIN 
+    users ON checkins.user_id = users.id
+WHERE 
+    users.user_type = 'staff' 
+    AND DATE_FORMAT(checkins.check_in_at, '%Y%m') = '202507'  -- 対象年月を指定
+ORDER BY 
+    checkins.check_in_at DESC;
